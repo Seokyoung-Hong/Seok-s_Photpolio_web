a536c61857bc22d015c8adea38ee237790ba4150
홍석영 <tjrdud0330@naver.com>
2022-05-25 09:38:09 +0900

    buttons

--- pics_maker.py
+++ pics_maker.py
@@ -1,5 +1,7 @@
-f = open("htmls.txt", 'w')
-for i in range(1, 187):
-    data = f"<img src=\"images/{i}_1.jpg\" class=\"img_set\">\n"
-    f.write(data)
-f.close()
+
+for j in range(10):
+    f = open(f"texts/html_{j+1}.txt", 'w')
+    for i in range(1,21):
+        data = f"<img src=\"images/{j*20+i}_1.jpg\" class=\"img_set\">\n"
+        f.write(data)
+    f.close()
