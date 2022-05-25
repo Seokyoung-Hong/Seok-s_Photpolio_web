2c636e17a607113a3720e62ca9298e8cf8d44f76
홍석영 <tjrdud0330@naver.com>
2022-05-25 11:37:56 +0900

    add_images

--- pics_maker.py
+++ pics_maker.py
@@ -1,7 +1,7 @@
 
-for j in range(10):
+for j in range(22):
     f = open(f"texts/html_{j+1}.txt", 'w')
-    for i in range(1,21):
-        data = f"<img src=\"images/{j*20+i}_1.jpg\" class=\"img_set\">\n"
+    for i in range(0,15):
+        data = f"\t\t\t<img src=\"images/{j*15+i+1}_1.webp\" class=\"img_set\">\n"
         f.write(data)
     f.close()
