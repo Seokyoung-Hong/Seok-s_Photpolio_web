779787e5843d6d8f1c139ebbe535376bb34ed3eb
홍석영 <tjrdud0330@naver.com>
2022-05-27 11:35:26 +0900

    button_edited

--- html_edit copy.py
+++ html_edit copy.py
@@ -4,6 +4,55 @@ from PIL import Image
 from importlib_metadata import metadata
 
 
+def buttons(f,j,button_count):
+    f.write('        <div class="btn-group" role="group" aria-label="Basic radio toggle button group">\n')
+    f.write('            <div class="btn-group me-3" aria-label="Previous">\n')
+    # f.write('testsss')
+    if j == 0 :
+        f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{j+1}.html\'" disabled >Previous</button>\n')
+    else :
+        f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{j}.html\'" >Previous</button>\n')
+    f.write('            </div>\n')
+    f.write('            <div class="btn-group me-3" aria-label="First group"\n>')
+    
+    
+    if j < 5:
+        button_start = 1
+        button_end = 9
+        print('if')
+    
+    elif j > button_count-5 :
+        button_start = button_count - 9
+        button_end = button_count
+        print('else')
+    else :
+        button_start = j-3
+        button_end = j+5
+        print('elif')
+    for i in range(button_start,button_end+1):
+        if i == j :
+            f.write(f'\t\t\t\t<button type=\"button\" class=\"btn btn-primary\" onclick=\"location.href=\'index_page{i}.html\'\"disabled >{i}</button>\n')
+        
+        else :
+            f.write(f'\t\t\t\t<button type=\"button\" class=\"btn btn-primary\" onclick=\"location.href=\'index_page{i}.html\'\">{i}</button>\n')
+    f.write('        </div>\n')
+    f.write('            <div class="btn-group me-3" aria-label="next">\n')
+    if j == 0 :
+        f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{j+1}.html\'" disabled >Next</button>\n')
+    else :
+        f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{j+2}.html\'" >Next</button>\n')
+
+    f.write('            </div>\n')
+    f.write('        </div>\n')
+    
+    
+
+def html_copy(f,h,a,b) :
+    cmt = ''.join(h[a-1:b])
+    f.write(cmt)
+
+
+
 with open('index_page.html','r',encoding='UTF-8') as f :
     htmls = f.readlines()
     # print(htmls)
@@ -14,36 +63,11 @@ with open('index_page.html','r',encoding='UTF-8') as f :
 
 j=1
 with open(f'index_pagesss.html','w',encoding='UTF-8') as f :
-    cmt = ''.join(htmls[:22])
-    f.write(cmt)
-    # f.write('testsss')
-    if j == 0 :
-        f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{1}.html\'" disabled >Previous</button>\n')
-    else :
-        f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{1}.html\'" >Previous</button>\n')
     
-    cmt = ''.join(htmls[23:25])
-    print(cmt)
-    f.write(cmt)
+    html_copy(f,htmls,1,20)
+    buttons(f,j,22)
     
-    x = j+1
-    for i in range(1,22):
-        if i == x :
-            f.write(f'\t\t\t\t<button type=\"button\" class=\"btn btn-primary\" onclick=\"location.href=\'index_page{i}.html\'\"disabled >{i}</button>\n')
-        
-        else :
-            f.write(f'\t\t\t\t<button type=\"button\" class=\"btn btn-primary\" onclick=\"location.href=\'index_page{i}.html\'\">{i}</button>\n')
-    
-    cmt = ''.join(htmls[46:48])
-    f.write(cmt)
-    
-    if j==21 :
-        f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{1}.html\'" disabled >Next</button>\n')
-    else :
-        f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{1}.html\'" >Next</button>\n')
-    
-    cmt = ''.join(htmls[49:52])
-    f.write(cmt)
+    f.write('\t\t<p>\n')
     
     for i in range(1,16) :
         
@@ -53,40 +77,17 @@ with open(f'index_pagesss.html','w',encoding='UTF-8') as f :
         meta_data = img._getexif()
         list = meta_data[36867].split(' ')
         list2 = list[0].split(':')
-
-        f.write(f'\t\t\t\t<img src=\"images/{+i}_1.webp\" class=\"img_set\">\n')
-        f.write(f'\t\t\t\t<h6>{list2[0]} {list2[1]} {list2[2]}</h6>\n')
         
-    cmt = ''.join(htmls[67:72])
-    f.write(cmt)
-    
-    if j == 0 :
-        f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{1}.html\'" disabled >Previous</button>\n')
-    else :
-        f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{1}.html\'" >Previous</button>\n')
-    
-    cmt = ''.join(htmls[73:75])
-    f.write(cmt)
-    
-    
-    for i in range(1,22):
-        if i == x :
-            f.write(f'\t\t\t\t<button type=\"button\" class=\"btn btn-primary\" onclick=\"location.href=\'index_page{i}.html\'\"disabled >{i}</button>\n')
+        f.write(f'\t\t\t\t<img src=\"images/{j*15+i}_1.webp\" class=\"img_set\">\n')
+        f.write(f'\t\t\t\t<h6>\n\t\t\t\t\t{list2[0]} {list2[1]} {list2[2]}\n')
+        f.write(f'\t\t\t\t\t<a href="images/{j*15+i}_1.webp" target="_blank">open image bigger</a>\n')
+        f.write('\t\t\t\t</h6>\n')
         
-        else :
-            f.write(f'\t\t\t\t<button type=\"button\" class=\"btn btn-primary\" onclick=\"location.href=\'index_page{i}.html\'\">{i}</button>\n')
+    html_copy(f,htmls,68,71)
     
-    cmt = ''.join(htmls[96:98])
-    f.write(cmt)
+    buttons(f,j,22)
     
-    if j==21 :
-        f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{j}.html\'" disabled >Next</button>\n')
-    else :
-        f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{j}.html\'" >Next</button>\n')
-    
-    
-    cmt = ''.join(htmls[99:])
-    f.write(cmt)
+    html_copy(f,htmls,102,109)
         
     
 
--- html_edit.py
+++ html_edit.py
@@ -4,48 +4,70 @@ from PIL import Image
 from importlib_metadata import metadata
 
 
+def buttons(f,j,button_count):
+    f.write('        <div class="btn-group" role="group" aria-label="Basic radio toggle button group">\n')
+    f.write('            <div class="btn-group me-3" aria-label="Previous">\n')
+    # f.write('testsss')
+    if j == 0 :
+        f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{j+1}.html\'" disabled >Previous</button>\n')
+    else :
+        f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{j}.html\'" >Previous</button>\n')
+    f.write('            </div>\n')
+    f.write('            <div class="btn-group me-3" aria-label="First group"\n>')
+    
+    
+    if j < 5:
+        button_start = 1
+        button_end = 9
+        # print('if')
+    
+    elif j > button_count-5 :
+        button_start = button_count - 8
+        button_end = button_count
+        # print('else')
+    else :
+        button_start = j-3
+        button_end = j+5
+        # print('elif')
+    for i in range(button_start,button_end+1):
+        if i == j+1 :
+            f.write(f'\t\t\t\t<button type=\"button\" class=\"btn btn-primary\" onclick=\"location.href=\'index_page{i}.html\'\"disabled >{i}</button>\n')
+        
+        else :
+            f.write(f'\t\t\t\t<button type=\"button\" class=\"btn btn-primary\" onclick=\"location.href=\'index_page{i}.html\'\">{i}</button>\n')
+    f.write('        </div>\n')
+    f.write('            <div class="btn-group me-3" aria-label="next">\n')
+    if j+1 == button_end :
+        f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{j+1}.html\'" disabled >Next</button>\n')
+    else :
+        f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{j+2}.html\'" >Next</button>\n')
+
+    f.write('            </div>\n')
+    f.write('        </div>\n')
+    
+    
+
+def html_copy(f,h,a,b) :
+    cmt = ''.join(h[a-1:b])
+    f.write(cmt)
+
+
+
+with open('index_page.html','r',encoding='UTF-8') as f :
+    htmls = f.readlines()
+    # print(htmls)
+
+
+
+# print(''.join(htmls[42:44]))
 
 for j in range(22):
-    with open('index_page.html','r',encoding='UTF-8') as f :
-        htmls = f.readlines()
-        # print(htmls)
-
-
-
-    # print(''.join(htmls[42:44]))
-
-    
     with open(f'index_page{j+1}.html','w',encoding='UTF-8') as f :
-        cmt = ''.join(htmls[:22])
-        f.write(cmt)
-        # f.write('testsss')
-        if j == 0 :
-            f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{j}.html\'" disabled >Previous</button>\n')
-        else :
-            f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{j-1}.html\'" >Previous</button>\n')
         
-        cmt = ''.join(htmls[23:25])
-        # print(cmt)
-        f.write(cmt)
+        html_copy(f,htmls,1,20)
+        buttons(f,j,21)
         
-        x = j+1
-        for i in range(1,22):
-            if i == x :
-                f.write(f'\t\t\t\t<button type=\"button\" class=\"btn btn-primary\" onclick=\"location.href=\'index_page{i}.html\'\"disabled >{i}</button>\n')
-            
-            else :
-                f.write(f'\t\t\t\t<button type=\"button\" class=\"btn btn-primary\" onclick=\"location.href=\'index_page{i}.html\'\">{i}</button>\n')
-        
-        cmt = ''.join(htmls[46:48])
-        f.write(cmt)
-        
-        if j==21 :
-            f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{j}.html\'" disabled >Next</button>\n')
-        else :
-            f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{j+1}.html\'" >Next</button>\n')
-        
-        cmt = ''.join(htmls[49:52])
-        f.write(cmt)
+        f.write('\t\t<p>\n')
         
         for i in range(1,16) :
             
@@ -55,52 +77,14 @@ for j in range(22):
             meta_data = img._getexif()
             list = meta_data[36867].split(' ')
             list2 = list[0].split(':')
-
+            
             f.write(f'\t\t\t\t<img src=\"images/{j*15+i}_1.webp\" class=\"img_set\">\n')
-            f.write(f'\t\t\t\t<h6>{list2[0]} {list2[1]} {list2[2]}</h6>\n')
+            f.write(f'\t\t\t\t<h6>\n\t\t\t\t\t{list2[0]} {list2[1]} {list2[2]}\n')
+            f.write(f'\t\t\t\t\t<a href="images/{j*15+i}_1.webp" target="_blank">open image bigger</a>\n')
+            f.write('\t\t\t\t</h6>\n')
             
-        cmt = ''.join(htmls[67:72])
-        f.write(cmt)
+        html_copy(f,htmls,68,71)
         
-        if j == 0 :
-            f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{j}.html\'" disabled >Previous</button>\n')
-        else :
-            f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{j-1}.html\'" >Previous</button>\n')
+        buttons(f,j,21)
         
-        cmt = ''.join(htmls[73:75])
-        f.write(cmt)
-        
-        
-        for i in range(1,22):
-            if i == x :
-                f.write(f'\t\t\t\t<button type=\"button\" class=\"btn btn-primary\" onclick=\"location.href=\'index_page{i}.html\'\"disabled >{i}</button>\n')
-            
-            else :
-                f.write(f'\t\t\t\t<button type=\"button\" class=\"btn btn-primary\" onclick=\"location.href=\'index_page{i}.html\'\">{i}</button>\n')
-        
-        cmt = ''.join(htmls[96:98])
-        f.write(cmt)
-        
-        if j==21 :
-            f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{j}.html\'" disabled >Next</button>\n')
-        else :
-            f.write(f'\t\t\t\t<button type="button" class="btn btn-primary" onclick="location.href=\'index_page{j+1}.html\'" >Next</button>\n')
-        
-        
-        cmt = ''.join(htmls[99:])
-        f.write(cmt)
-        
-    
-
-# print(htmls[62])
-
-
-# "\n".join(htmls)
-
-        
-    
-
-# print(htmls[62])
-
-
-# "\n".join(htmls)
+        html_copy(f,htmls,102,109)
